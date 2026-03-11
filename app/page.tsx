3ff8dd65bc6fa5d390a496f858e43b1d6c3b9a52
joaopimentamt-hub <joaopimentamt@gmail.com>
2026-03-11 14:51:24 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
-export default function Page() {
-  return (
-    <main>
-      <h1>Pimenta Mídias</h1>
-      <p>Site em construção</p>
-    </main>
-  )
-}
+import Navbar from "@/Navbar";
+import Hero from "@/Hero";
+import Problems from "@/Problems";
+import Solution from "@/Solution";
+import HowItWorks from "@/HowItWorks";
+import Benefits from "@/Benefits";
+import Process from "@/Process";
+import CTA from "@/CTA";
+import Footer from "@/Footer";
+import WhatsAppFloat from "@/WhatsAppFloat";
+
+export default function Home() {
+    return (
+          <main>
+                <Navbar />
+                <Hero />
+                <Problems />
+                <Solution />
+                <HowItWorks />
+                <Benefits />
+                <Process />
+                <CTA />
+                <Footer />
+                <WhatsAppFloat />
+          </main>main>
+        );
+}</main>
