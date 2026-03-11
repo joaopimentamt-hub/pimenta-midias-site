ddf2b95e54a976ecf4d20a836f78aee5ed4faece
joaopimentamt-hub <joaopimentamt@gmail.com>
2026-03-11 14:49:29 -0300

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,27 +1,27 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import Problems from "@/components/Problems";
-import Solution from "@/components/Solution";
-import HowItWorks from "@/components/HowItWorks";
-import Benefits from "@/components/Benefits";
-import Process from "@/components/Process";
-import CTA from "@/components/CTA";
-import Footer from "@/components/Footer";
-import WhatsAppFloat from "@/components/WhatsAppFloat";
+import Navbar from "@/Navbar";
+import Hero from "@/Hero";
+import Problems from "@/Problems";
+  import Solution from "@/Solution";
+import HowItWorks from "@/HowItWorks";
+import Benefits from "@/Benefits";
+import Process from "@/Process";
+import CTA from "@/CTA";
+import Footer from "@/Footer";
+import WhatsAppFloat from "@/WhatsAppFloat";
 
 export default function Home() {
-  return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Problems />
-      <Solution />
-      <HowItWorks />
-      <Benefits />
-      <Process />
-      <CTA />
-      <Footer />
-      <WhatsAppFloat />
-    </main>
-  );
-}
+    return (
+          <main>
+                <Navbar />
+                <Hero />
+                <Problems />
+                <Solution />
+                <HowItWorks />
+                <Benefits />
+                <Process />
+                <CTA />
+                <Footer />
+                <WhatsAppFloat />
+          </main>main>
+        );
+}</main>
